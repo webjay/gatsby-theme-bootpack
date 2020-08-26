e1b3932ab510d14fd2539d9d38fcb8c7f873d8d4
Jacob Friis Saxberg <jacob@weareheadlight.com>
2020-08-26 14:00:15 +0200

    v1

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+# Gatsby Bootstrap Theme
+
+In `gatsby-config.js`:
+
+Add `gatsby-theme-bootstrap` to `plugins`.
---
+++ gatsby-node.js
@@ -0,0 +1,53 @@
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+
+function onCreateWebpackConfig({ actions, stage, loaders }) {
+  const config = {
+    plugins: [
+      new webpack.ProvidePlugin({
+        jQuery: 'jquery',
+        $: 'jquery',
+        jquery: 'jquery',
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(scss)$/,
+          use: [{
+            loader: 'style-loader', // inject CSS to page
+          }, {
+            loader: 'css-loader', // translates CSS into CommonJS modules
+            options: {
+              esModule: false,
+            },
+          }, {
+            loader: 'postcss-loader', // Run postcss actions
+            options: {
+              // postcss plugins, can be exported to postcss.config.js
+              plugins: () => [autoprefixer],
+            },
+          }, {
+            // compiles Sass to CSS
+            loader: 'sass-loader',
+          }],
+        },
+      ],
+    },
+  };
+  if (stage === 'build-html') {
+    config.module.rules.push(
+      {
+        test: require.resolve('bootstrap'),
+        use: loaders.null(),
+      },
+      // {
+      //   test: require.resolve('bootstrap/js/dist/util'),
+      //   use: loaders.null(),
+      // },
+    );
+  }
+  actions.setWebpackConfig(config);
+}
+
+exports.onCreateWebpackConfig = onCreateWebpackConfig;
